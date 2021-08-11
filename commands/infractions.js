e66b032328375f6d2105511d33c570dca2c23b70
JRMi27 <69006354+JRMi27@users.noreply.github.com>
2021-08-11 06:04:35 +0200

    Add files via upload

--- commands/infractions.js
+++ commands/infractions.js
@@ -8,9 +8,9 @@ module.exports = {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
         const member = message.mentions.members.first()
         if (!member) return message.channel.send('Veuillez mentionner le membre dont voir les warns.')
-        if (!client.db.warns[member.id]) return message.channel.send('Ce membre n\'a aucun warn.')
+        if (!client.warning.warns[member.id]) return message.channel.send('Ce membre n\'a aucun warn.')
         message.channel.send(new Discord.MessageEmbed()
-            .setDescription(`**Total de warns :** ${client.db.warns[member.id].length}\n\n__**10 derniers warns**__\n\n${client.db.warns[member.id].slice(0, 10).map((warn, i) => `**${i + 1}.** ${warn.reason}\nSanctionné ${moment(warn.date).fromNow()} par <@!${warn.mod}>`).join('\n\n')}`))
+            .setDescription(`**Total de warns :** ${client.warning.warns[member.id].length}\n\n__**10 derniers warns**__\n\n${client.warning.warns[member.id].slice(0, 10).map((warn, i) => `**${i + 1}.** ${warn.reason}\nSanctionné ${moment(warn.date).fromNow()} par <@!${warn.mod}>`).join('\n\n')}`))
     },
     name: 'infractions',
     guildOnly: true
